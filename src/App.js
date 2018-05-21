f52606680e9b0bdaaf97c205010b021323f6b3dc
Andrew Stout <andrew@andrewstout.me>
2018-05-21 19:47:21 -0400

    Added react-arcgis, map, and marker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Map } from 'react-arcgis';
+
 import logo from './logo.svg';
 import './App.css';
+import Marker from './Marker';
 
 class App extends Component {
   render() {
@@ -13,6 +16,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div style={{ height: '50vh' }}>
+          <Map>
+            <Marker
+              position={{ latitude: 37.758431, longitude: -122.426622 }}
+            />
+          </Map>
+        </div>
       </div>
     );
   }
---
+++ src/Marker.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { loadModules } from 'react-arcgis';
+
+export default class Marker extends React.Component {
+  componentWillMount() {
+    const { latitude, longitude } = this.props.position;
+    loadModules(['esri/Graphic'])
+      .then(([Graphic]) => {
+        // Add the geometry and symbol to a new graphic
+        const graphic = new Graphic({
+          geometry: {
+            type: 'point', // auto-casts as new Point()
+            longitude,
+            latitude,
+          },
+          symbol: {
+            type: 'simple-marker', // auto-casts as new SimpleMarkerSymbol()
+            color: [226, 119, 40],
+            outline: {
+              // auto-casts as new SimpleLineSymbol()
+              color: [255, 255, 255],
+              width: 2,
+            },
+          },
+        });
+
+        this.setState({ graphic });
+        this.props.view.graphics.add(graphic);
+      })
+      .catch(err => console.error(err));
+  }
+
+  componentWillUnmount() {
+    this.props.view.graphics.remove(this.state.graphic);
+  }
+  render() {
+    return null;
+  }
+}
